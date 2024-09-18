7ec461b305d89fe3f50d789f023503453820a7aa
Shawn Sutherland <shawnsutherland@hotmail.com>
2024-09-18 20:54:25 +0200

    Fixed 'delete account' button on edit profile.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  has_and_belongs_to_many :attempts, join_table: 'attempts_answers'
+  has_and_belongs_to_many :attempts, join_table: 'attempts_answers', dependent: :destroy
+
+  before_destroy :remove_attempts_with_chosen_answer
+
+  private
+
+  def remove_attempts_with_chosen_answer
+    Attempt.where(chosen_answer_id: self.id).destroy_all
+  end
+
 end
