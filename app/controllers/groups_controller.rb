bad437f34c2e59e9fd510370fa46918baa33dd3c
Shawn Sutherland <shawnsutherland@hotmail.com>
2024-10-16 12:04:20 +0200

    Can now create groups from existing groups.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,18 @@
 class GroupsController < ApplicationController
   def new
     @group = Group.new
+    @groups = Group.all
   end
 
   def create
-    @group = Group.new(group_params)
-    @group.teacher = current_user
+    @group = current_user.owned_groups.new(group_params)
+
+    # If groups were selected in the "Select all students from an existing group" dropdown
+    if params[:group_ids].present?
+      selected_groups = Group.where(id: params[:group_ids])
+      selected_students = selected_groups.flat_map(&:students).uniq # Get all students from the selected groups
+      @group.students += selected_students # Add them to the new group
+    end
 
     if @group.save
       redirect_to dashboard_my_groups_path, notice: "Group created successfully!"
