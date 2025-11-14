1f4633db6f55edf119eb454fa59a743a7a120641
Roel de Jong <Roel4811@hotmail.com>
2025-11-14 15:14:49 +0100

    Update courses_helper.rb

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -7,4 +7,10 @@ module CoursesHelper
 
     link_to(index + 1, course_question_path(course, question), class: classes.join(" "))
   end
+
+  def levels_list(course)
+    return "" if course.levels.empty?
+
+    "(#{course.levels.map(&:name).join(", ")})"
+  end
 end
